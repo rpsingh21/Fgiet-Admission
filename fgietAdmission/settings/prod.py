fa6e224d432b509eba01857843b263d9c2dc1bb5
Rohit Prakash Singh <rohitprakashsingh21@gmail.com>
2018-05-08 01:37:21 +0530

    improve settings

--- fgietAdmission/settings/prod.py
+++ fgietAdmission/settings/prod.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'j2b_z(*4w+#)t^nz3)0n3da(tcj&3##klo73m76(x7%3z)b%85n!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
