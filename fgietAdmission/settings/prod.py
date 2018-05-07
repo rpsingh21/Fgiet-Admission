b0aaec4f233db244b10db4d9369a3b8e4e744ed3
Rohit Prakash Singh <rohitprakashsingh21@gmail.com>
2018-05-07 23:59:43 +0530

    update production settings

--- fgietAdmission/settings/prod.py
+++ fgietAdmission/settings/prod.py
@@ -4,12 +4,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*.fgiet.in']
 
 # Application definition
 
